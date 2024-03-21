254fb540574a6211f929fd4f38748024b26bd127
Nur Hadi <decoding0506@gmail.com>
2024-03-21 23:14:30 +0700

    add gear features

--- app/(root)/gears/page.tsx
+++ app/(root)/gears/page.tsx
@@ -1,8 +1,10 @@
-// import GearCategoryFilter from "@/components/shared/GearCategoryFilter"
-// import GearCollection from "@/components/shared/GearCollection"
-// import Search from "@/components/shared/Search"
+import GearCategoryFilter from "@/components/shared/GearCategoryFilter"
+import GearCollection from "@/components/shared/GearCollection"
+import Search from "@/components/shared/Search"
+import { Button } from "@/components/ui/button"
 import { getAllGears } from "@/lib/actions/gear.actions"
 import { SearchParamProps } from "@/types"
+import Link from "next/link"
  
 const Gears = async ({ searchParams }: SearchParamProps) => {
     const page = Number(searchParams?.page) || 1;
@@ -18,12 +20,21 @@ const Gears = async ({ searchParams }: SearchParamProps) => {
 
     return (
         <>
-            <section className="wrapper my-8 flex flex-col md:gap-12">
-                <h2 className="h2-bold">Gears</h2>
-                {/* <div className="flex w-full flex-col gap-5 py-5 md:flex-row">
-                    <Search placeholder="Search Product"/>
+            <section className="bg-primary-50 bg-dotted-pattern bg-cover bg-center py-5 md:py-10">
+                <div className="flex wrapper justify-between flex-row">
+                    <h2 className="h2-bold">Gears</h2>
+                    <Link href="gears/create" >
+                        <Button className="rounded-full">
+                            + Create
+                        </Button>
+                    </Link>
+                </div>
+                <div className="flex wrapper flex-col gap-5 py-5 md:flex-row">
+                    <Search placeholder="Search Gears"/>
                     <GearCategoryFilter/>
                 </div>
+            </section>
+            <div className="wrapper my-8">
                 <GearCollection
                     data={gears?.data}
                     emptyTitle="No Gears Found"
@@ -32,8 +43,8 @@ const Gears = async ({ searchParams }: SearchParamProps) => {
                     limit={15}
                     page={page}
                     totalPages={gears?.totalPages}
-                /> */}
-            </section>
+                />
+            </div>
             </>
     )
 }
